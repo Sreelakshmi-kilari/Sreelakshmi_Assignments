c556f4c49588c644507c51031aeca7919abbc3e5
Sreelakshmi-kilari <105065607+Sreelakshmi-kilari@users.noreply.github.com>
2022-05-28 12:04:22 +0530

    Added File

---
+++ Annotations/java/custom.java
@@ -0,0 +1,22 @@
+package Annotations.java;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@interface info{
+    int authorID();
+    String author() default "Null";
+    String supervisor() default "Null";
+    String date();
+    String time();
+    int version();
+    String description() default "General class";
+}
+
+
+@info(authorID = 12, date = "21 April", time = "10pm", version = 6)
+public class custom {
+
+}
+
